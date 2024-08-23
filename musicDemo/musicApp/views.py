89b03924cd773460f72d167dd3e24046b3f1d057
maxbartrip <maximillianbartrip@yahoo.co.uk>
2024-08-23 01:37:36 +0100

    Added alphabetical sorting of songs

--- musicDemo/musicApp/views.py
+++ musicDemo/musicApp/views.py
@@ -6,5 +6,14 @@ def main(request):
     return render(request, "main.html")
 
 def playlist(request):
-    songs = Song.objects.all()
-    return render(request, "playlist.html", {'songs': songs})
+    sort_option = request.GET.get('sort', 'name_a_z')
+
+    if sort_option == 'name_z_a':
+        songs = Song.objects.order_by('-title')  # Sort by title Z-A
+    else:
+        songs = Song.objects.order_by('title')   # Sort by title A-Z
+
+    for song in songs:
+        song.sorted_genres = sorted(song.genres.all(), key=lambda g: g.name)
+
+    return render(request, "playlist.html", {'songs': songs, 'sort_option': sort_option})
